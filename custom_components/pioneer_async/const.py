06850b2987ad244949032fe260bcb140c16cff6b
Crowbar Z <crowbarz@outlook.com>
2024-01-14 19:35:30 +0000

    Bump to 0.8.5 in const.py as well

--- custom_components/pioneer_async/const.py
+++ custom_components/pioneer_async/const.py
@@ -20,7 +20,7 @@ from homeassistant.const import (
 
 DOMAIN = "pioneer_async"
 PLATFORMS = ["media_player"]
-VERSION = "0.8.4"
+VERSION = "0.8.5"
 
 DEFAULT_HOST = "avr"
 DEFAULT_NAME = "Pioneer AVR"
